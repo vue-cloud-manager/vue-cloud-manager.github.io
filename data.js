bbee1ddb51599fd43edf555e9b6f287c643e2a5e
Simati-PC\Simati <simonenko744@gmail.com>
2018-01-22 21:57:11 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,151 @@
+var data = {
+  id: 0,
+  name: 'VUE-CLOUD-MANAGER',
+  createTime: '',
+  createDate: '',
+  modifiedTime: '',
+  modifiedDate: '',
+  owner: '',
+  format: 'root',
+  share: '',
+  data: '',
+  children: [
+    {
+      id: 1,
+      name: 'Fork Template',
+      createTime: '19:00:00',
+      createDate: '23.12.2017',
+      modifiedTime: '19:00:00',
+      modifiedDate: '23.12.2017',
+      owner: 'me',
+      format: 'folder',
+      share: 'none',
+      data: '',
+      children: [{
+        id: 2,
+        name: 'Fork Template 2 level',
+        createTime: '19:03:00',
+        createDate: '24.12.2017',
+        modifiedTime: '19:03:00',
+        modifiedDate: '24.12.2017',
+        owner: 'me',
+        format: 'folder',
+        share: 'none',
+        data: '',
+        children: [{
+          id: 3,
+          name: 'Fork Template 3 level',
+          createTime: '19:03:05',
+          createDate: '24.12.2016',
+          modifiedTime: '19:03:05',
+          modifiedDate: '24.12.2016',
+          owner: 'me',
+          format: 'folder',
+          share: 'none',
+          data: '',
+          children: [{
+            id: 4,
+            name: 'Fork Template 4 level',
+            createTime: '11:03:05',
+            createDate: '24.10.2016',
+            modifiedTime: '11:03:05',
+            modifiedDate: '24.10.2016',
+            owner: 'me',
+            format: 'folder',
+            share: 'none',
+            data: '',
+            children: [{
+              id: 5,
+              name: 'Fork Template 5 level',
+              createTime: '11:31:05',
+              createDate: '24.11.2016',
+              modifiedTime: '11:31:05',
+              modifiedDate: '24.11.2016',
+              owner: 'me',
+              format: 'folder',
+              share: 'none',
+              data: '',
+              children: [{
+                id: 6,
+                name: 'Fork Template 6 level',
+                createTime: '11:31:05',
+                createDate: '02.10.2016',
+                modifiedTime: '11:31:05',
+                modifiedDate: '02.10.2016',
+                owner: 'me',
+                format: 'folder',
+                share: 'none',
+                data: '',
+                children: []
+              }]
+            }]
+          }]
+        }]
+      },
+      {
+        id: 7,
+        name: 'Collection 2 level',
+        createTime: '11:31:05',
+        createDate: '02.10.2016',
+        modifiedTime: '11:31:05',
+        modifiedDate: '02.11.2016',
+        owner: 'me',
+        format: 'folder',
+        share: 'none',
+        data: '',
+        children: []
+      },
+      {
+        id: 8,
+        name: 'Fork item',
+        createTime: '11:31:05',
+        createDate: '03.11.2016',
+        modifiedTime: '11:31:05',
+        modifiedDate: '03.11.2016',
+        owner: 'me',
+        format: 'file',
+        share: 'none',
+        data: ''
+      }
+      ]
+    },
+    {
+      id: 9,
+      name: 'Draft',
+      createTime: '07:07:05',
+      createDate: '03.11.2010',
+      modifiedTime: '07:07:05',
+      modifiedDate: '03.11.2010',
+      owner: 'me',
+      format: 'folder',
+      share: 'none',
+      data: '',
+      children: [
+        {
+          id: 10,
+          name: 'Draft file',
+          createTime: '07:08:59',
+          createDate: '03.11.2010',
+          modifiedTime: '07:08:59',
+          modifiedDate: '03.11.2010',
+          owner: 'me',
+          format: 'file',
+          share: 'none',
+          data: ''
+        }
+      ]
+    },
+    {
+      id: 11,
+      name: 'root file',
+      createTime: '07:18:59',
+      createDate: '03.11.2010',
+      modifiedTime: '07:18:59',
+      modifiedDate: '03.11.2010',
+      owner: 'me',
+      format: 'file',
+      share: 'none',
+      data: ''
+    }
+  ]
+}
